8c7e53d56281395d54b5932a548bff1096236651
amirhossein.f <dj1372@gmail.com>
2024-01-31 18:14:09 +0330

    commit on 01/31/2024 18:14

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from accounts.models import Profile, Panel
+from django.contrib.admin.helpers import ACTION_CHECKBOX_NAME
+from django.contrib.auth.models import User
+from django.db.models import Q
 
+from accounts.models import Profile, Panel
+from utils.utils import random_name, random_password
 
 @admin.register(Panel)
 class PanelAdmin(admin.ModelAdmin):
@@ -19,24 +23,44 @@ class PanelAdmin(admin.ModelAdmin):
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    actions_on_top = True
+
     list_display = (
-        'user',
+        'username',
+        'password',
         'panel',
         'created_at_display',
         'updated_at_display',
+        'created_by',
+    )
+
+    list_filter = (
+        'user__is_staff',
     )
 
     readonly_fields = (
+        'username',
         'created_at',
         'updated_at',
+        'created_by',
     )
 
     fields = (
-        'user',
+        'username',
+        'password',
         'panel',
         'created_at',
         'updated_at',
+        'created_by',
     )
+
+    def save_model(self, request, instance, form, change):
+        instance = form.save(commit=False)
+        if not change:
+            instance.created_by = request.user
+        instance.save()
+        form.save_m2m()
+        return instance
 
     @admin.display(description="تاریخ ایجاد", empty_value='???')
     def created_at_display(self, obj):
@@ -47,3 +71,40 @@ class ProfileAdmin(admin.ModelAdmin):
     def updated_at_display(self, obj):
         data_time = str(obj.updated_at.strftime('%Y-%m-%d - %H:%M'))
         return data_time
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            q = Q()
+            q &= Q(**{f'user': request.user})
+            q |= (Q(user__is_staff=False) & Q(created_by=request.user))
+            qs = qs.filter(q)
+            pass
+        return qs
+
+    def has_add_permission(self, request):
+        return False
+
+    def changelist_view(self, request, extra_context=None):
+        if 'action' in request.POST and request.POST['action'] == 'create_profile':
+            if not request.POST.getlist(ACTION_CHECKBOX_NAME):
+                post = request.POST.copy()
+                for u in Profile.objects.all():
+                    post.update({ACTION_CHECKBOX_NAME: str(u.id)})
+                request._set_post(post)
+        return super(ProfileAdmin, self).changelist_view(request, extra_context)
+
+    @admin.action(description='ساخت کاربر')
+    def create_profile(self, request, queryset):
+        print(1)
+        username = random_name()
+        password = random_password()
+        new_user = User.objects.create_user(username=username)
+        new_user.set_password(password)
+        Profile.objects.create(user=new_user, username=username, password=password, created_by=request.user)
+
+    create_profile.acts_on_all = True
+
+    actions = (
+        'create_profile',
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django_jalali.db import models as jmodel
 
@@ -20,9 +20,12 @@ class Panel(models.Model):
 
 class Profile(models.Model):
     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE, verbose_name="کاربر")
+    username = models.CharField(max_length=255, null=True, blank=True, editable=False, verbose_name='نام کاربری')
+    password = models.CharField(max_length=255, null=True, blank=True, verbose_name='کلمه عبور')
     panel = models.ForeignKey(Panel, null=True, blank=True, on_delete=models.PROTECT, verbose_name="پنل")
     created_at = jmodel.jDateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
     updated_at = jmodel.jDateTimeField(auto_now=True, verbose_name='تاریخ بروز رسانی')
+    created_by = models.ForeignKey(User, related_name='profile_created_by', null=False, blank=False, on_delete=models.CASCADE, verbose_name="ساخته شده توسط")
 
     def __str__(self):
         return self.user.username
@@ -31,8 +34,33 @@ class Profile(models.Model):
         verbose_name = 'پروفایل'
         verbose_name_plural = 'پروفایل ها'
 
+    def save(self, *args, **kwargs):
+        if self.user and self.password:
+            user = self.user
+            user.set_password(self.password)
+            user.save()
+        super().save(*args, **kwargs)
+
 
 @receiver(post_save, sender=User)
 def auto_create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+    if instance.is_staff or instance.is_superuser:
+        try:
+            Profile.objects.get(user=instance)
+        except:
+            Profile.objects.create(user=instance, username=instance.username, created_by=User.objects.get(username='admin'))
+
+
+
+@receiver(post_delete, sender=Profile)
+def auto_delete_user_on_profile_delete(sender, instance, **kwargs):
+    try:
+        instance.user.delete()
+        related_profiles = Profile.objects.filter(created_by=instance.user).delete()
+        for related_profile in related_profiles:
+            try:
+                related_profile.user.delete()
+            except:
+                pass
+    except:
+        pass
